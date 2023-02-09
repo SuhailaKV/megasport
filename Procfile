8bae198fdc9fed65029d4495c5249c120cd4c407
suhailaKV <suhailamufeedh@gmail.com>
2023-02-09 22:53:09 +0400

    new commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn megasport.wsgi --log-file -
--- megasport/settings.py
+++ megasport/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#$m6ds6ca4@2cl+0kilgdd-8+58jxj@rvk^_%@snezb)4^e67_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -126,8 +126,8 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
